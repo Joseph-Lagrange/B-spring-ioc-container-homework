c6c07fa864c0c35ee6cbe28961c571c759862e8a
wudibin <781721794@qq.com>
2020-11-08 10:52:24 +0800

    [wu] solve Problem 3 S2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +8,8 @@ public class Foo {
 
     private Bar bar;
 
-    public Foo(@Lazy Bar bar) {
+    @Autowired
+    public void setBar(Bar bar) {
         this.bar = bar;
     }
 
